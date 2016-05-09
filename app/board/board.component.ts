87884d3fa2fe239dcd8fce34f3205bbed9ffcec0
Alejandro Arroyo Duque <Alejandro Arroyo Duque>
2016-05-09 10:18:32 +0200

    feat(cell): agrega component cell

--- app/board/board.component.ts
+++ app/board/board.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { CellComponent } from '../cell/cell.component';
+
 @Component({
   selector: 'bs-board',
+  directives: [CellComponent], 
   templateUrl: 'app/board/board.component.html'
 })
 export class BoardComponent { }
---
+++ app/cell/cell.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'bs-cell',
+  templateUrl: 'app/cell/cell.component.html'
+})
+export class CellComponent { }
