5e1584b44cebf422c0237032fe68427e6fcb63f2
Alejandro Arroyo Duque <Alejandro Arroyo Duque>
2016-05-12 11:46:55 +0200

    feat(validaciones): agrega validaciones al formulario

--- app/modes/one-player/one-player.component.ts
+++ app/modes/one-player/one-player.component.ts
@@ -1,5 +1,5 @@
-import { Component, Output, EventEmitter, OnChanges, SimpleChange } from '@angular/core';
-import { FORM_DIRECTIVES, FORM_BINDINGS, ControlGroup, FormBuilder } from '@angular/common';
+import { Component, Output, EventEmitter, OnInit, SimpleChange } from '@angular/core';
+import { FORM_DIRECTIVES, FORM_BINDINGS, ControlGroup, FormBuilder, Validators } from '@angular/common';
 import { MissionService } from '../../shared/mission.service';
 import { BoardComponent } from '../../board/board.component';
 
@@ -14,7 +14,7 @@ import { PHASE } from '../phase';
   templateUrl: 'app/modes/one-player/one-player.component.html',
   styleUrls: ['app/modes/one-player/one-player.component.css']
 })
-export class OnePlayerComponent {
+export class OnePlayerComponent implements OnInit {
   boardSize: number = 10;
   form: ControlGroup;
   turns: number = 0;
@@ -22,20 +22,25 @@ export class OnePlayerComponent {
   phase: string = PHASE.main;
   units: number = 5;
   //
-  constructor(private missionService: MissionService, formBuilder: FormBuilder) {
+  constructor(private missionService: MissionService, private formBuilder: FormBuilder) {
     missionService.addShipConfirmed$.subscribe(
       ship => {
         console.log(`Esta es la confirmacion desde one-player ${ship}`)
       })
-    this.form = formBuilder.group({
-      coordinateX: '',
-      coordinateY: ''
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      coordinateX: ['', Validators.compose([Validators.required, Validators.pattern('^[aA-zZ]{1}$')])],
+      coordinateY: ['', Validators.compose([Validators.required, Validators.pattern('^\\d{1,2}$')])]
     });
 
     this.form.valueChanges.subscribe(data => {
+      console.log(this.form)
       data.coordinateX = data.coordinateX.toUpperCase();
     });
   }
+
   announce(x, y, units, orientation) {
     console.log('announce()')
     let xParsed = x.charCodeAt(0) - 65,
