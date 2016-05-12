2097204d1037c8bae3b48a47be52d326e71d2867
Alejandro Arroyo Duque <Alejandro Arroyo Duque>
2016-05-12 10:06:21 +0200

    fix(form): vigila cambios en formulario de coordenadas

--- app/modes/one-player/one-player.component.ts
+++ app/modes/one-player/one-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnChanges, SimpleChange } from '@angular/core';
+import { FORM_DIRECTIVES, FORM_BINDINGS, ControlGroup, FormBuilder } from '@angular/common';
 import { MissionService } from '../../shared/mission.service';
 import { BoardComponent } from '../../board/board.component';
 
@@ -8,22 +9,32 @@ import { PHASE } from '../phase';
 
 @Component({
   selector: 'bs-one-player',
-  directives: [BoardComponent, IaComponent],
+  directives: [FORM_DIRECTIVES, BoardComponent, IaComponent],
   providers: [MissionService],
-  templateUrl: 'app/modes/one-player/one-player.component.html'
+  templateUrl: 'app/modes/one-player/one-player.component.html',
+  styleUrls: ['app/modes/one-player/one-player.component.css']
 })
 export class OnePlayerComponent {
   boardSize: number = 10;
+  form: ControlGroup;
   turns: number = 0;
   orientation: string = ORIENTATION.vertical;
   phase: string = PHASE.main;
   units: number = 5;
   //
-  constructor(private missionService: MissionService) {
+  constructor(private missionService: MissionService, formBuilder: FormBuilder) {
     missionService.addShipConfirmed$.subscribe(
       ship => {
         console.log(`Esta es la confirmacion desde one-player ${ship}`)
       })
+    this.form = formBuilder.group({
+      coordinateX: '',
+      coordinateY: ''
+    });
+
+    this.form.valueChanges.subscribe(data => {
+      data.coordinateX = data.coordinateX.toUpperCase();
+    });
   }
   announce(x, y, units, orientation) {
     console.log('announce()')
@@ -31,6 +42,10 @@ export class OnePlayerComponent {
         yParsed = parseInt(y);
 
     this.missionService.announceAddShip({x: xParsed, y: yParsed, units, orientation});
+  }
+
+  changeX() {
+    console.log(this.form);
   }
 
   addTurn() {
