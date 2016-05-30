d1ec0b917e86550e4ea556f2052a49ae03ca8e0b
Alejandro Arroyo Duque <alexandrosAD90@gmail.com>
2016-05-30 12:15:20 +0200

    feat(tableros): si es de ia no muestra posicion barcos

--- app/cell/cell.component.ts
+++ app/cell/cell.component.ts
@@ -4,6 +4,7 @@ import { Cell } from './cell';
 @Component({
   selector: 'bs-cell',
   templateUrl: 'app/cell/cell.component.html',
+  inputs: ['isIa'],
   styleUrls: ['app/cell/cell.component.css']
 })
 export class CellComponent {
